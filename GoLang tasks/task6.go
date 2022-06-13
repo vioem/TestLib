32529df71a6703ac9ea45ae46d1e33dd9e1765bb
dreamer <vioem@ya.ru>
2022-06-13 18:40:57 +0400

    add new tasks2

--- GoLang tasks/task6.go
+++ GoLang tasks/task6.go
@@ -1,2 +1,27 @@
 // Дан массив целых чисел из 200 элементов. Массив заполняется на основе датчика случайных чисел от 0 до 1000.
 // Подсчитать количество четных и нечетных значений этого массива.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var odd, even int
+
+func main() {
+	arr :=make([]int, 200)
+	rand.Seed(time.Now().UnixNano())
+	for i:=0; i<len(arr); i++ {
+		arr[i]=rand.Intn(1000)
+	}
+	for i, _ :=range arr {
+		if arr[i]%2==0 {
+			even++
+		} else {
+			odd++
+		}
+	}
+fmt.Printf("В массиве %d чётных и %d нечётных значений", even, odd)
+}
